e73878f570f451944a8e2d8736dd1eee9f5cc41d
filipe <filipe.lins01@gmail.com>
2021-05-23 23:04:31 -0400

    implementando listagem de produtos

--- meli-app/components/Products-list/index.tsx
+++ meli-app/components/Products-list/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 const ProductList = (props: any) => {
     return(
         <section className={styles.listContainer}>
-            
+            <div className={styles.container}></div>
         </section>
     )
 }
